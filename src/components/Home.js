61480752e5b7c4342eac3808c2b196640c1340ed
Thibault <contact@thibaultmeunier.com>
2019-03-31 20:55:56 +0100

    :art: Homepage landing screen implemented and eslint

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Search from './Search';
-import TitleHeader from "./layout/TitleHeader";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Search from './Search'
+import TitleHeader from './layout/TitleHeader'
+import workerImg from '../assets/img/worker.svg'
 
 export default function Home() {
-    return (
-        <div>
-            <section className="hero is-light is-fullheight-with-navbar">
-                <TitleHeader/>
-                <div className="hero-body">
-                    <div className="container has-text-centered">
-                        <h1 className="title is-2">
-                            Getting started
-                        </h1>
-                        <div className='content'>
-
-                            <Link className='button is-medium is-success' to='/start'>Start with introduction</Link>
-                            <p className='mt30'>
-                                Already familiar with blockchain?
-                                <strong>
-                                    <a href='#advanced' className='is-link has-text-link'> Try the advanced search for topics</a>
-                                </strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <Search/>
+  return (
+    <div>
+      <section className="hero is-light is-fullheight hero-header-component">
+        {/* <TitleHeader/> */}
+        <div className="hero-body has-text-white">
+          <div className="container has-text-centered">
+            <img src={workerImg} className={'image landing-page-image'} />
+            <h1 className={'title is-1 has-text-white is-cursive is-extra-bold'}>
+              Forge Your Blockchain Skills
+            </h1>
+            <h2 className={'is-2 has-text-white'}>
+              Blockchain theory curated by Universities, Interactive online IDE, No certification fee
+            </h2>
+            <div className={'content'} />
+            <div className="buttons" style={{ display: 'block' }}>
+              <Link className="button is-light is-big" to={'/start'}>
+                <span className="icon">
+                  <i className="fas fa-graduation-cap" />
+                </span>
+                <span>Beginner</span>
+              </Link>
+              <Link className={'button is-dark is-big'} to={'start'}>
+                <span className="icon">
+                  <i className="fas fa-terminal" />
+                </span>
+                <span>Expert</span>
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+      </section>
+      <Search />
+    </div>
+  )
 }
