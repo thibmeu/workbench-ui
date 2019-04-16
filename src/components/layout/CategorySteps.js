a75921e1e3e1911361425105972105c4a904129d
Thibault <contact@thibaultmeunier.com>
2019-04-16 18:35:02 +0100

    :recycle: Refactor Category steps

--- src/components/layout/CategorySteps.js
+++ src/components/layout/CategorySteps.js
@@ -1,68 +1,62 @@
-import React from 'react';
-import {withRouter} from "react-router";
-import connect from "react-redux/es/connect/connect";
-import {Link} from "react-router-dom";
-import {urlify} from "../../lib/helpers";
+import React from 'react'
+import { withRouter } from 'react-router'
+import connect from 'react-redux/es/connect/connect'
+import { Link } from 'react-router-dom'
+import { urlify } from '../../lib/helpers'
 
 class CategorySteps extends React.Component {
-
-    getStepsForPages(category, pages) {
-        const steps = [];
-        if (pages) {
-            for (const page of pages) {
-                if (page.url.endsWith('/')) {
-                    steps.push(this.getOverviewStep(category));
-                } else {
-                steps.push(<li key={page.url}
-                               className={`steps-segment ${this.props.match.params.page && this.props.match.params.page.toLowerCase() === urlify(page.title.toLowerCase())
-                                   ? 'is-active' : ''}`}>
-                    <Link to={`/pages/${category}/${urlify(page.title)}`}>
-                        <span className="steps-marker"/>
-                        <div className="steps-content">
-                            <p className="is-size-7">{page.title}</p>
-                        </div>
-                    </Link>
-                </li>)}
-            }
-        }
-        return steps;
+  getStepsForPages(category, pages) {
+    if (!pages) {
+      return []
     }
+    const steps = []
 
-    getOverviewStep(categoryName) {
-        return (<li key='overview' className={`steps-segment ${!this.props.match.params.page ? 'is-active' : ''}`}>
-            <Link to={`/pages/${categoryName}`}>
-                <span className="steps-marker">
-                    <span className="icon"><i className="fa fa-circle is-size-7"/></span>
+    for (const page of pages) {
+      const isOverview = page.url.endsWith('/')
+      const title = isOverview ? 'Overview' : page.title
+      const url = `/pages/${category}` + !isOverview ? `/${urlify(title)}` : ''
+
+      steps.push(
+        <li key={page.url} className={`steps-segment ${this.isActivePage(page, isOverview) ? 'is-active' : ''}`}>
+          <Link to={url}>
+            <span className="steps-marker">
+              {isOverview ? (
+                <span className="icon">
+                  <i className="fa fa-circle is-size-7" />
                 </span>
-                <div className="steps-content"><p className="is-size-7">Overview</p></div>
-            </Link>
-        </li>)
+              ) : null}
+            </span>
+            <div className="steps-content">
+              <p className="is-size-7">{title}</p>
+            </div>
+          </Link>
+        </li>,
+      )
     }
+    return steps
+  }
 
-    getSummaryStep(categoryName) {
-        return (<li className={`steps-segment ${this.props.match.params.page === 'summary' ? 'is-active' : ''}`}>
-            <Link to={`/pages/${categoryName}/summary`}>
-                <span className="steps-marker">
-                    <span className="icon"><i className="fa fa-check is-size-7"/></span>
-                </span>
-                <div className="steps-content"><p className="is-size-7">Summary</p></div>
-            </Link>
-        </li>)
-    }
+  render() {
+    let activeCategoryName = this.props.match.params.category
+    const activeCategoryPages = this.props.categories[activeCategoryName.toLowerCase()]
+    return (
+      <ul className="steps my-step-style has-content-centered is-hidden-mobile is-small">
+        {this.getStepsForPages(activeCategoryName, activeCategoryPages)}
+      </ul>
+    )
+  }
 
-    render() {
-        let activeCategoryName = this.props.match.params.category;
-        const activeCategoryPages = this.props.categories[activeCategoryName.toLowerCase()];
-        return (
-            <ul className="steps my-step-style has-content-centered is-hidden-mobile is-small">
-                {this.getStepsForPages(activeCategoryName, activeCategoryPages)}
-            </ul>)
-    }
+  isActivePage(page, isOverview) {
+    return (
+      (isOverview && !this.props.match.params.page) ||
+      (this.props.match.params.page && this.props.match.params.page.toLowerCase() === urlify(page.title.toLowerCase()))
+    )
+  }
 }
 
-const mapStateToProps = state => {
-    return {categories: state.categories}
-};
+const mapStateToProps = state => ({
+  categories: state.categories,
+})
 
-const ConnectedCategorySteps = connect(mapStateToProps)(CategorySteps);
-export default withRouter(ConnectedCategorySteps);
+const ConnectedCategorySteps = connect(mapStateToProps)(CategorySteps)
+export default withRouter(ConnectedCategorySteps)
