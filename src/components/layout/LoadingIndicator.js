c52a9302ba9bc342cd0ed4f031f17075e2b1bfea
Thibault <contact@thibaultmeunier.com>
2019-04-16 13:41:30 +0100

    :recycle: Loading indicator eslint

--- src/components/layout/LoadingIndicator.js
+++ src/components/layout/LoadingIndicator.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
 
 class LoadingIndicator extends React.Component {
-    render() {
-        return (this.props.loading
-            ? <p className='icon loading has-text-info'><i className='fas fa-spinner fa-spin'/></p>
-            : null);
-    }
+  render() {
+    return this.props.loading ? (
+      <p className={'icon loading has-text-info'}>
+        <i className={'fas fa-spinner fa-spin'} />
+      </p>
+    ) : null
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        loading: state.appState.pages.loading
-    };
-};
+const mapStateToProps = state => {
+  return {
+    loading: state.appState.pages.loading,
+  }
+}
 
-export default connect(mapStateToProps)(LoadingIndicator);
+export default connect(mapStateToProps)(LoadingIndicator)
