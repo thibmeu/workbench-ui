58762a01694bbe3a0e6d462d5ea58711912edf88
Thibault <contact@thibaultmeunier.com>
2019-04-18 15:56:45 +0100

    :art: Error 404 page designed
     #19

--- src/components/page/Page404.js
+++ src/components/page/Page404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import error404Picture from '../../assets/img/undraw_404_error.svg'
 import TitleHeader from '../layout/TitleHeader'
 
@@ -7,13 +8,25 @@ export class Page404 extends React.Component {
     return (
       <div className={'is-fullheight'}>
         <TitleHeader />
-        <section>
-          <figure className={'image'}>
-            <img src={error404Picture} />
-          </figure>
-          {/* Explain this is 404 not found */}
-          {/* button to go back to homepage */}
-        </section>
+        <div className={'container'}>
+          <section className="section" style={{ marginBottom: '-3rem' }}>
+            <div className="container">
+              <div className="columns is-vcentered">
+                <div className="column feature-box">
+                  <h1 className={'title'}>Ooops!</h1>
+                  <h2 className={'subtitle'}>We can't find the page you are looking for</h2>
+
+                  <Link className={'button is-info is-big'} to={'/'}>
+                    Take me Home
+                  </Link>
+                </div>
+                <div className="column">
+                  <img className={'image image-box border-shadow'} src={error404Picture} height={200} alt={''} />
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     )
   }
